6513f8371ab39b60b6430cec4640003d2cee421a
srikanth555 <srikanthmaragoni555@gmail.com>
2021-12-16 20:50:03 +0530

    second commit

---
+++ src/Sri/Url.java
@@ -0,0 +1,23 @@
+package Sri;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Url {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.setProperty("webdriver.chrome.driver","E://Drivers/chromedriver.exe" );
+			WebDriver  driver=new ChromeDriver();
+		
+			
+		}
+
+		private static void driver() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+
